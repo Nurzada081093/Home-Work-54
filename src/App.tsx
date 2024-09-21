7f96e3cab0657f2764a35c6f541cbebc453cf38b
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-09-21 22:44:30 +0600

    Init App!

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+const App = () => (
+    <div className='home-work'>
+        <p style={{backgroundColor: 'green', color: 'whitesmoke', width: '300px'}}>I'm starting my home work #54!</p>
+        <div style={{fontSize: '30px'}}>^_^</div>
+    </div>
+);
+
+export default App;
