1d3230cfa4fc7aa579fed51b7fb872fbe6dceb4c
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-09-22 00:24:49 +0600

    Создание функции createItems в файле App.tsx и прописание логики создания массива с объектами hasItem и clicked. А так же получение рандомного объекта с содержанием предмета.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import './App.css';
 
-const App = () => (
-    <div className='home-work'>
-        <p style={{backgroundColor: 'green', color: 'whitesmoke', width: '300px'}}>I'm starting my home work #54!</p>
-        <div style={{fontSize: '30px'}}>^_^</div>
-    </div>
-);
+interface Items {
+    hasItem: boolean;
+    clicked: boolean;
+}
+
+const App = () => {
+
+    const createItems = () => {
+        const arrayItems: Items[] = [];
+
+        for (let i:number = 0; i < 36; i++) {
+           arrayItems.push({clicked: false, hasItem: false});
+        }
+
+        const randomItem:number = Math.floor(Math.random() * arrayItems.length);
+        console.log(randomItem);
+
+        for (let i:number = 0; i < arrayItems.length; i++) {
+            if (i === randomItem) {
+                arrayItems[i].hasItem = true;
+                console.log(i);
+                console.log(arrayItems[i]);
+            }
+        }
+
+        return arrayItems;
+    };
+
+    console.log(createItems());
+
+
+    return (
+        <div className="container">
+        </div>
+    );
+};
 
 export default App;
